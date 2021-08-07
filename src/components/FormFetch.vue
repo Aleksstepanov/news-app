1a4723e5b42c59e99925e806e862cc3a0635a8fd
Aleksey Stepanov <stepanovl87@gmail.com>
2021-08-07 17:41:23 +0300

    spinner components

--- src/components/FormFetch.vue
+++ src/components/FormFetch.vue
@@ -1,47 +1,69 @@
 <template>
-  <b-row class="mr-0 ml-0">
-    <b-form @submit.prevent="submitClickHandler()" inline>
-      <b-form-group id="label-search" label="Search" label-for="search">
-        <b-form-input
-          id="search"
-          v-model="form.search"
-          type="text"
-          placeholder="Enter search target"
-          required
-        ></b-form-input>
-      </b-form-group>
+  <b-row class="mr-0 ml-0 mt-2 mb-2">
+    <b-form @submit.prevent="submitClickHandler()">
+      <b-row class="mr-0 ml-0">
+        <b-form-group
+          id="label-search"
+          label="News topics:"
+          label-for="search"
+          class="w-50"
+        >
+          <b-form-input
+            id="search"
+            v-model="form.search"
+            type="text"
+            placeholder="Enter search target"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </b-row>
+      <b-row class="mr-0 ml-0">
+        <b-form-group
+          class="w-50 mr-2"
+          id="label-from"
+          label="Date of first article:"
+          label-for="from"
+        >
+          <b-form-datepicker id="from" v-model="form.from"></b-form-datepicker>
+        </b-form-group>
 
-      <b-form-group id="label-from" label="From" label-for="from">
-        <b-form-datepicker
-          id="from"
-          v-model="form.from"
-          placeholder="date of first article"
-        ></b-form-datepicker>
-      </b-form-group>
+        <b-form-group
+          id="label-to"
+          class="w-50"
+          label="Date of last article:"
+          label-for="to"
+        >
+          <b-form-datepicker id="to" v-model="form.to"></b-form-datepicker>
+        </b-form-group>
+      </b-row>
 
-      <b-form-group id="label-to" label="To" label-for="to">
-        <b-form-datepicker
-          id="to"
-          v-model="form.to"
-          placeholder="date of last article"
-        ></b-form-datepicker>
-      </b-form-group>
+      <b-row class="mr-0 ml-0">
+        <b-form-group
+          id="label-language"
+          class="w-25 mr-2"
+          label="Language:"
+          label-for="language"
+        >
+          <b-form-select
+            id="language"
+            v-model="form.language"
+            :options="languages"
+          ></b-form-select>
+        </b-form-group>
 
-      <b-form-group id="label-language" label="Language" label-for="language">
-        <b-form-select
-          id="language"
-          v-model="form.language"
-          :options="languages"
-        ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="label-sort" label="Sort by..." label-for="sortBy">
-        <b-form-select
-          id="sortBy"
-          v-model="form.sortBy"
-          :options="sortOptions"
-        ></b-form-select>
-      </b-form-group>
+        <b-form-group
+          class="w-25"
+          id="label-sort"
+          label="Sort by:"
+          label-for="sortBy"
+        >
+          <b-form-select
+            id="sortBy"
+            v-model="form.sortBy"
+            :options="sortOptions"
+          ></b-form-select>
+        </b-form-group>
+      </b-row>
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
   </b-row>
@@ -58,8 +80,8 @@ export default {
       form: {
         title: "",
         search: "",
-        from: "",
-        to: "",
+        from: new Date(),
+        to: new Date(),
         language: "en",
         sortBy: "publishedAt",
       },
---
+++ src/components/Spiner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="d-flex justify-content-center mb-3 w-100">
+    <b-spinner variant="primary" label="Spinning"></b-spinner>
+  </div>
+</template>
